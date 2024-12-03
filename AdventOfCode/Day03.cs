c7a1afe68ec486db7a93c57793083fd173e26983
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-12-03 20:00:50 +0100

    day 3 part 1

---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+internal class Day03 : BaseDay
+{
+    private readonly string _input;
+
+    public Day03()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        long result = 0;
+
+        MatchCollection matches = Regex.Matches(_input, @"mul\([0-9]+,[0-9]+\)");
+        foreach (Match match in matches)
+        {
+            string value = match.Captures[0].Value;
+            string[] parts = value.Split(new[] { "mul(", ",", ")" }, StringSplitOptions.RemoveEmptyEntries);
+            result += long.Parse(parts[0]) * long.Parse(parts[1]);
+        }
+
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("");
+    }
+}
