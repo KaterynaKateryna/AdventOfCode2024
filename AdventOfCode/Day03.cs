c9cc77e4f62c4a96cb938f85a8c80727af2bdbf2
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-12-03 20:16:21 +0100

    day 3 part 2

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -29,6 +29,30 @@ internal class Day03 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("");
+        long result = 0;
+
+        MatchCollection matches = Regex.Matches(_input, @"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)");
+
+        bool enabled = true;
+        foreach (Match match in matches)
+        {
+            string value = match.Captures[0].Value;
+
+            if (value == "do()")
+            {
+                enabled = true;
+            }
+            else if (value == "don't()")
+            {
+                enabled = false;
+            }
+            else if (enabled)
+            {
+                string[] parts = value.Split(new[] { "mul(", ",", ")" }, StringSplitOptions.RemoveEmptyEntries);
+                result += long.Parse(parts[0]) * long.Parse(parts[1]);
+            }
+        }
+
+        return new(result.ToString());
     }
 }
