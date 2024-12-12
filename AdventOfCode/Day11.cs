f2e0646b348702064592a189238913c5d00ffb93
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-12-12 08:35:51 +0100

    day 11: add recursive attempt

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -31,16 +31,21 @@ public class Day11 : BaseDay
         List<long> result = new List<long>();
         for (int i = 0; i < stones.Count; i++)
         {
-            result.AddRange(Blink(stones[i]));
+            (long a, long? b) = Blink(stones[i]);
+            result.Add(a);
+            if (b.HasValue)
+            {
+                result.Add(b.Value);
+            }
         }
         return result;
     }
 
-    private List<long> Blink(long stone)
+    private (long a, long? b) Blink(long stone)
     {
         if (stone == 0)
         {
-            return new List<long> { 1L };
+            return (1L, null);
         }
         else if (stone.ToString().Length % 2 == 0)
         {
@@ -48,19 +53,50 @@ public class Day11 : BaseDay
             long one = long.Parse(stoneNumberString.Substring(0, stoneNumberString.Length / 2));
             long two = long.Parse(stoneNumberString.Substring(stoneNumberString.Length / 2));
 
-            return new List<long> { one, two };
+            return (one, two);
         }
         else
         {
-            return new List<long> { stone * 2024L };
+            return (stone * 2024L, null);
         }
     }
 
     private List<long> GetInput()
-    { 
+    {
         return File.ReadAllText(InputFilePath)
             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
             .Select(long.Parse)
             .ToList();
     }
+
+    #region recursive
+
+    private long BlinkRecursive(List<long> stones, int times)
+    {
+        long result = 0;
+        for (int i = 0; i < stones.Count; i++)
+        {
+            result += BlinkRecursive(stones[i], times);
+        }
+        return result;
+    }
+
+    private long BlinkRecursive(long stone, int times)
+    {
+        (long a, long? b) = Blink(stone);
+
+        if (times == 1)
+        {
+            return b.HasValue ? 2 : 1;
+        }
+
+        long result = BlinkRecursive(a, times - 1);
+        if (b.HasValue)
+        {
+            result += BlinkRecursive(b.Value, times - 1);
+        }
+        return result;
+    }
+
+    #endregion
 }
