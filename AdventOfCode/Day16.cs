7967857affac30e20c88cbc64ea70c680c794eb0
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-12-16 12:44:46 +0100

    day 16 part 1

---
+++ AdventOfCode/Day16.cs
@@ -0,0 +1,165 @@
+﻿
+namespace AdventOfCode;
+
+internal class Day16 : BaseDay
+{
+    private (char[][] map, Position start, Position end) Init()
+    {
+        char[][] map = File.ReadAllLines(InputFilePath).Select(x => x.ToCharArray()).ToArray();
+
+        Position start = default!;
+        Position end = default!;
+
+        for (int i = 0; i < map.Length; ++i)
+        {
+            for (int j = 0; j < map[i].Length; ++j)
+            {
+                if (map[i][j] == 'S')
+                {
+                    start = new Position(i, j);
+                }
+                if (map[i][j] == 'E')
+                {
+                    end = new Position(i, j);
+                }
+            }
+        }
+
+        return (map, start, end);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        (char[][] map, Position start, Position end) = Init();
+
+        long[][] costs = new long[map.Length][];
+        for (int i = 0; i < map.Length; ++i)
+        {
+            costs[i] = new long[map[i].Length];
+            for (int j = 0; j < map[i].Length; ++j)
+            {
+                if (i == start.I && j == start.J)
+                {
+                    costs[i][j] = 0;
+                }
+                else
+                {
+                    costs[i][j] = long.MaxValue;
+                }
+            }
+        }
+
+        GetCost(map, costs, start, 0, end, Direction.East);
+
+        long cost = costs[end.I][end.J];
+
+        return new(cost.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("");
+    }
+
+    private void GetCost(char[][] map, long[][] costs, Position current, long currentCost, Position end, Direction direction)
+    {
+        if (current == end)
+        {
+            return;
+        }
+
+        Position straight = GetNext(current, direction);
+        if (map[straight.I][straight.J] != '#')
+        {
+            if (currentCost + 1 < costs[straight.I][straight.J])
+            {
+                costs[straight.I][straight.J] = currentCost + 1;
+
+                GetCost(map, costs, straight, currentCost + 1, end, direction);
+            }
+        }
+
+        Position clockwise = GetNext(current, RotateClockwise(direction));
+        if (map[clockwise.I][clockwise.J] != '#')
+        {
+            if (currentCost + 1001 < costs[clockwise.I][clockwise.J])
+            {
+                costs[clockwise.I][clockwise.J] = currentCost + 1001;
+
+                GetCost(map, costs, clockwise, currentCost + 1001, end, RotateClockwise(direction));
+            }
+        }
+
+        Position counter = GetNext(current, RotateCounterClockwise(direction));
+        if (map[counter.I][counter.J] != '#')
+        {
+            if (currentCost + 1001 < costs[counter.I][counter.J])
+            {
+                costs[counter.I][counter.J] = currentCost + 1001;
+
+                GetCost(map, costs, counter, currentCost + 1001, end, RotateCounterClockwise(direction));
+            }
+        }
+    }
+
+    private Direction RotateClockwise(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.East:
+                return Direction.South;
+            case Direction.South:
+                return Direction.West;
+            case Direction.West:
+                return Direction.North;
+            case Direction.North:
+                return Direction.East;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private Direction RotateCounterClockwise(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.East:
+                return Direction.North;
+            case Direction.South:
+                return Direction.East;
+            case Direction.West:
+                return Direction.South;
+            case Direction.North:
+                return Direction.West;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private Position GetNext(Position from, Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.North:
+                return new Position(from.I - 1, from.J);
+            case Direction.South:
+                return new Position(from.I + 1, from.J);
+            case Direction.East:
+                return new Position(from.I, from.J + 1);
+            case Direction.West:
+                return new Position(from.I, from.J - 1);
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private record Position(int I, int J);
+
+    private enum Direction
+    { 
+        East,
+        West,
+        North,
+        South
+    }
+}
