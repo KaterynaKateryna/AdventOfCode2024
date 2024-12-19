6e7fd2e08ca393b7a691d6e13fd9413c8875611c
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-12-19 18:23:40 +0100

    day 19 WIP

--- AdventOfCode/Day19.cs
+++ AdventOfCode/Day19.cs
@@ -5,6 +5,8 @@ public class Day19 : BaseDay
     string[] _towels;
     string[] _patterns;
 
+    Dictionary<char, List<string>> _towelsByFirstLetter = new Dictionary<char, List<string>>();
+
     public Day19()
     {
         string[] lines = File.ReadAllLines(InputFilePath);
@@ -14,6 +16,14 @@ public class Day19 : BaseDay
         for (int i = 2; i < lines.Length; ++i)
         { 
             _patterns[i-2] = lines[i].Trim();
+        }
+
+        foreach (string towel in _towels) 
+        {
+            if (!_towelsByFirstLetter.TryAdd(towel[0], new List<string> { towel }))
+            {
+                _towelsByFirstLetter[towel[0]].Add(towel);
+            }
         }
     }
 
@@ -26,7 +36,11 @@ public class Day19 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new("test");
+        int result = _patterns.Sum(p => CountPatterns(p, 0));
+
+        //int result = CountPatterns(_patterns[0], 0);
+
+        return new(result.ToString());
     }
 
     private bool IsPatternPossible(string pattern, int position)
@@ -57,5 +71,39 @@ public class Day19 : BaseDay
         }
 
         return false;
+    }
+
+    private int CountPatterns(string pattern, int position)
+    {
+        if (position == pattern.Length)
+        {
+            return 1;
+        }
+
+        int res = 0;
+        if (_towelsByFirstLetter.TryGetValue(pattern[position], out var towelsToCheck))
+        {
+            foreach (string towel in towelsToCheck)
+            {
+                if (towel.Length <= pattern.Length - position)
+                {
+                    bool matches = true;
+                    for (int i = 0; i < towel.Length; ++i)
+                    {
+                        if (pattern[position + i] != towel[i])
+                        {
+                            matches = false;
+                            break;
+                        }
+                    }
+                    if (matches)
+                    {
+                        res += CountPatterns(pattern, position + towel.Length);
+                    }
+                }
+            }
+        }
+
+        return res;
     }
 }
