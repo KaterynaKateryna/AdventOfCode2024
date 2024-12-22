2804a9ced3ffa0df178621e0c5930b1cb380f2f8
KaterynaKateryna <tok.ar.tok@gmail.com>
2024-12-22 22:25:21 +0100

    day 22 part 1

---
+++ AdventOfCode/Day22.cs
@@ -0,0 +1,39 @@
+﻿
+namespace AdventOfCode;
+
+public class Day22 : BaseDay
+{
+    public override ValueTask<string> Solve_1()
+    {
+        long[] numbers = File.ReadAllLines(InputFilePath).Select(long.Parse).ToArray();
+
+        long result = numbers.Sum(n => GetIteration(n, 2000));
+
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new("");
+    }
+
+    private long GetIteration(long number, int iterations)
+    {
+        for (int i = 0; i < iterations; ++i)
+        {
+            // 1
+            number = (number << 6) ^ number;
+            number = number % 16777216;
+
+            // 2
+            number = (number / 32) ^ number;
+            number = number % 16777216;
+
+            // 3
+            number = (number << 11) ^ number;
+            number = number % 16777216;
+        }
+
+        return number;
+    }
+}
